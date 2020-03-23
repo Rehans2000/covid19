72dab7dec835b1ef8f83f932148b09cf38ff755b
muhammad rehan <rehans2k3@gmail.com>
2020-03-23 19:48:37 +0500

    initial commit

---
+++ models/highriskassessment.model.js
@@ -0,0 +1,15 @@
+﻿const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let highrisks = new Schema({
+    question: {
+        type: String
+    },
+    type: {
+        type: String
+    },
+    choices: {
+        type: Array
+    }
+});
+module.exports = mongoose.model('highrisks', highrisks);
