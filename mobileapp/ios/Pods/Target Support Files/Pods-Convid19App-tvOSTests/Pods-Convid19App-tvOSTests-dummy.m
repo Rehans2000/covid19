ab28d3e64ad747c71e9ad22624da4e6bfc0eed07
muhammad rehan <rehans2k3@gmail.com>
2020-03-23 15:29:16 +0500

    initial commit

--- mobileapp/ios/Pods/Target Support Files/Pods-Convid19App-tvOSTests/Pods-Convid19App-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Convid19App_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_Convid19App_tvOSTests
-@end
--- mobileapp/ios/Pods/Target Support Files/Pods-Convid19AppTests/Pods-Convid19AppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Convid19AppTests : NSObject
-@end
-@implementation PodsDummy_Pods_Convid19AppTests
-@end
